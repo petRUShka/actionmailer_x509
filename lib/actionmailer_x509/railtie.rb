389eac84df67784c07e293c88249ed0c166e68c0
petrushka <petrushkin@yandex.ru>
2011-07-31 10:37:39 +0400

    Fix: remove deprecated railtie_name

--- lib/actionmailer_x509/railtie.rb
+++ lib/actionmailer_x509/railtie.rb
@@ -2,8 +2,6 @@ require 'actionmailer_x509'
 require 'rails'
 module ActionMailerX509
   class Railtie < Rails::Railtie
-    railtie_name :actionmailer_x509
-
     rake_tasks do
       load "tasks/actionmailer_x509.rake"
       load "tasks/tiny_performance_test.rake"
